68aaf67ef348464b414450a164dafa6e3824537d
Harry <ddembrs@gmail.com>
2023-05-24 19:03:46 +0800

    build

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(process.env.NODE_ENV === "production" ? "/pokevue/" : "/"),
+  history: createWebHistory(process.env.NODE_ENV === "production" ? "/its-show-time/" : "/"),
   routes: [
     {
       path: "/",
