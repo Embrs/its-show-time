a7f15a0b99d92c16e9aff187223bb2310336044b
Harry <ddembrs@gmail.com>
2023-05-30 17:29:09 +0800

    auto routes complete

--- src/router/_auto-bgm-pages.ts
+++ src/router/_auto-bgm-pages.ts
@@ -3,10 +3,10 @@ const routesPaths = import.meta.glob("@/views/bgm/pages/**/*.vue");
 
 // 資料準備
 const routes = await Object.keys(routesPaths).map((routesPath) => {
-  const path: string = routesPath.replace("/src/views/bgm/pages/", "").replace(".vue", "");
+  const path: string = routesPath.replace("/src/views/bgm/pages/", "/bgm/").replace(".vue", "");
   console.log(`@/views/bgm/pages/${path}.vue`);
   return {
-    path: path === "home" ? "/bgm/" : `/bgm/${path}`,
+    path: path === "bgm/" ? "/bgm/" : `/${path}`,
     name: path.replace(/\//g, "-"),
     component: routesPaths[routesPath]
   };
