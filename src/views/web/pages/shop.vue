5a3048a58407a18e67025ed99b4e46d8931c6357
Embers <ddembrs@gmail.com>
2023-06-26 13:51:05 +0800

    shop

---
+++ src/views/web/pages/shop.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+//- 購物
+#Shop
+  p Shop
+</template>
+
+<script setup lang="ts">
+// script
+</script>
+
+<style lang="scss" scoped>
+// 佈局
+#Shop {
+}
+// 組件
+#Shop {
+}
+</style>
