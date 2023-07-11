ab41c87e488dede9d8c75e8e2f1147ffce110946
Embers <ddembrs@gmail.com>
2023-07-11 16:30:50 +0800

    dialog basic

--- src/views/bgm/pages/com/dialog.vue
+++ src/views/bgm/pages/com/dialog.vue
@@ -2,18 +2,27 @@
 //- 請填寫功能描述👈
 #Dialog
   Todo(text="彈窗與對話框")
+  div(ref="container")
 </template>
 
 <script setup lang="ts">
 import Todo from "@/components/module/todo.vue";
-// import { h, } from "vue";
+import { render, ref, onMounted, h } from "vue";
 
-// const vnode = h("div", { class: "my-class" }, "Hello World");
-// const render = () =>  {
-//   return  h("div", { class: "my-class" }, "Hello World");
-// };
-// render();
-// script
+// 在onMounted钩子中渲染 vNode
+const container = ref();
+onMounted(() => {
+
+  // 创建一个vNode
+  const vNode = h("div", { class: "red" }, "Hello, Vue 3!");
+
+  // 渲染vNode到容器中
+  // 注意：需要将容器元素的引用传递给渲染函数的第二个参数
+  // 渲染函数会将vNode渲染到容器中
+  const el = document.querySelector("#BgmLayout");
+  if (!el) return;
+  render(vNode, el);
+});
 </script>
 
 <style lang="scss" scoped>
