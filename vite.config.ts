f23ab4529a2781f7a9debdc41472b4bbf97f74ed
Embers <ddembrs@gmail.com>
2023-07-24 11:18:54 +0800

    get version

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 // import fs from "fs"; // https use (打包請關閉)
 // import path from "path"; // https use (打包請關閉)
+// get version
+// const { version } = JSON.parse(fs.readFileSync("package.json", "utf-8"));
+// console.log(version);
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
